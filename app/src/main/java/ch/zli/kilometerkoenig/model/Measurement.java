755311c35be7a60e4197b9535b797ad1fe2c7ab0
noahlauper <noahlauper@gmail.com>
2024-03-18 11:15:39 +0100

    added attributes, constructor and getter, setters to the Measurement class

--- app/src/main/java/ch/zli/kilometerkoenig/model/Measurement.java
+++ app/src/main/java/ch/zli/kilometerkoenig/model/Measurement.java
@@ -1,3 +1,60 @@
 package ch.zli.kilometerkoenig.model;
+
+import java.time.Instant;
+
 public class Measurement {
+
+    private Long id;
+    private Instant startTime;
+    private Instant endTime;
+    private int steps;
+    private int lvlPoints;
+
+    public Measurement(Long id, Instant startTime, Instant endTime, int steps, int lvlPoints) {
+        this.id = id;
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.steps = steps;
+        this.lvlPoints = lvlPoints;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(Instant startTime) {
+        this.startTime = startTime;
+    }
+
+    public Instant getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(Instant endTime) {
+        this.endTime = endTime;
+    }
+
+    public int getSteps() {
+        return steps;
+    }
+
+    public void setSteps(int steps) {
+        this.steps = steps;
+    }
+
+    public int getLvlPoints() {
+        return lvlPoints;
+    }
+
+    public void setLvlPoints(int lvlPoints) {
+        this.lvlPoints = lvlPoints;
+    }
 }
