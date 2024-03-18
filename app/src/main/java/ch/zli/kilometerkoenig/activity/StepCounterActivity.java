17e4a532091a97eaaa9bfd00e47ad54d4827a0f5
noahlauper <noahlauper@gmail.com>
2024-03-18 14:42:15 +0100

    implemented SensorEventListener

--- app/src/main/java/ch/zli/kilometerkoenig/activity/StepCounterActivity.java
+++ app/src/main/java/ch/zli/kilometerkoenig/activity/StepCounterActivity.java
@@ -1,28 +1,45 @@
 package ch.zli.kilometerkoenig.activity;
 
+
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
+import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
+import android.Manifest;
+
 import ch.zli.kilometerkoenig.R;
 
-public class StepCounterActivity extends AppCompatActivity {
+public class StepCounterActivity extends AppCompatActivity implements SensorEventListener {
 
-    private final static int SAMPLING_RATE = 100000;
+    private final static int SAMPLING_RATE = 100;
+    private static final int REQUEST_CODE_ACTIVITY_RECOGNITION = 101;
 
+    private Button stopMeasurementButton;
     private SensorManager sensorManager;
     private Sensor sensor;
 
+    private TextView count;
+
+
+    @RequiresApi(api = Build.VERSION_CODES.Q)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,22 +50,33 @@ public class StepCounterActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        Button stopMeasurementButton = findViewById(R.id.stopMeasurement);
+        setView();
+        initializeSensor();
 
         stopMeasurementButton.setOnClickListener(view -> {
             Intent mainActivityIntend = new Intent(this, MainActivity.class);
             startActivity(mainActivityIntend);
         });
-
-        initializeSensor();
-
     }
 
+    private void setView() {
+        stopMeasurementButton = findViewById(R.id.stopMeasurement);
+        count = findViewById(R.id.stepCount);
+
+    }
 
     private void initializeSensor() {
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
         sensorManager.registerListener((SensorEventListener) this, sensor, SAMPLING_RATE);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
     }
 }
