332a7aa525f2fbf0abfade6f3d16599d90fbd1e3
noahlauper <noahlauper@gmail.com>
2024-03-19 08:35:12 +0100

    changed sdk to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,7 +8,7 @@ android {
 
     defaultConfig {
         applicationId = "ch.zli.kilometerkoenig"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
